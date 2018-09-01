4eb40a30973c3a48e07bb65b7be540a6f09afbf9
Harrison Shoebridge <harrison@theshoebridges.com>
2018-09-01 23:27:00 +1000

    Add basic build script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
+
+if [ ! $? -eq 0 ]; then
+    echo "For whatever reason, project isn't being build as a git repository. Assuming current directory is the project dir"
+
+    PROJECT_DIR=$(pwd)
+fi
+
+BUILD_DIR=$PROJECT_DIR/build
+
+$BUILD_DIR/basic
