bce32e4c0e2ef110f86ad13f84260eba0d82433c
Harrison Shoebridge <harrison@theshoebridges.com>
2018-09-21 20:42:10 +1000

    Add broken sin wave

--- src/test/audio.cpp
+++ src/test/audio.cpp
@@ -8,7 +8,12 @@ Uint32 wavLength;
 Uint8* wavBuffer;
 Uint8* wavHead;
 
+int sinBufferIndex = 0;
+int sinBufferSize = 0;
+Sint16* sinBuffer = 0;
+
 void audioCallback(void* userdata, Uint8* stream, int len) {
+  /*
   if (wavLength == 0) {
     printf("Done!\n");
 
@@ -29,7 +34,22 @@ void audioCallback(void* userdata, Uint8* stream, int len) {
   wavLength -= (Uint32) len;
 
   printf("wavLength: %d\n", wavLength);
-  printf("len: %d\n", len);
+  printf("len: %d\n", len);*/
+
+  len /= 2;
+  Sint16* buf = (Sint16*) stream;
+
+  for (int i = 0; i < len; i += 2) {
+    printf("audio?\n");
+    Sint16 v = sinBuffer[sinBufferIndex % sinBufferSize];
+
+    printf("%d\n", (int) v);
+
+    buf[i] = v;
+    buf[i + 1] = v;
+
+    sinBufferIndex += 1;
+  }
 }
 
 int main(int argc, char* argv[]) {
@@ -39,7 +59,7 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  SDL_LoadWAV("data/sound/eerie.wav", &wavSpec, &wavBuffer, &wavLength);
+  SDL_LoadWAV("data/sound/church.wav", &wavSpec, &wavBuffer, &wavLength);
 
   wavHead = wavBuffer;
   wavSpec.callback = audioCallback;
@@ -49,6 +69,26 @@ int main(int argc, char* argv[]) {
     printf("Failed to open audio: %s", SDL_GetError());
 
     return -1;
+  }
+
+  int samplesPerSecond = wavSpec.freq;
+
+  sinBufferSize = 2 * samplesPerSecond;
+  // `samplesPerSecond` for each channel (ie. 2)
+  sinBuffer = (Sint16*) malloc(sinBufferSize);
+
+  float phase = 0;
+  float phaseIncrement = samplesPerSecond/400.0f;
+
+  for (int i = 0; i < samplesPerSecond; i++) {
+    Sint16 v = 6000 * sin(2.0f * 3.1415 * i * wavSpec.freq / 200);
+
+    printf("%d\n", (int) v);
+
+    sinBuffer[i*2] = v;
+    sinBuffer[i*2 + 1] = v;
+
+    phase += phaseIncrement;
   }
 
   SDL_PauseAudioDevice(deviceID, 0);
